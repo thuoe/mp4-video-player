abf7e28945e6f22e538645a2d3bf18943ed72f90
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2020-04-11 00:41:33 +0100

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    extends: 'airbnb-base',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        'comma-dangle': [ 'error', 'never' ],
+        'no-underscore-dangle': ["error", { "allowAfterThis": true }]
+    }
+};
