cff5a46fdacdf2d0f8ab870f8ce5a9ac9d1348b1
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2020-04-12 18:24:37 +0100

    add extra lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-        mocha: true
-    },
-    extends: 'airbnb-base',
-    globals: {
-        assert: true,
-        expect: true,
-        fixture: true,
-        flush: true,
-        sinon: true
-    },
-    plugins: [ 'html' ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module'
-    },
-    rules: {
-        'comma-dangle': [ 'error', 'never' ],
-        'no-underscore-dangle': [ 'error', { 'allowAfterThis': true }],
-        'class-methods-use-this': 'off',
-        'max-len': 'off'
-    }
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    assert: true,
+    expect: true,
+    fixture: true,
+    flush: true,
+    sinon: true,
+    WCT: true
+  },
+  plugins: ['html'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { 'devDependencies': ['./demo/index.html'] }],
+    'comma-dangle': ['error', 'never'],
+    'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
+    'class-methods-use-this': 'off',
+    'max-len': 'off',
+  }
 };
