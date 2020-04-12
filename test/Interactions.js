7cfe16a0a60e7ad2f7dd5d3745cb49cff959a9e5
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2020-04-12 18:12:09 +0100

    fix linting errors in interaction.js

--- test/Interactions.js
+++ test/Interactions.js
@@ -1,42 +1,39 @@
+/* eslint-disable no-param-reassign */
 /**
- * Simulates a complete 'space' key press by firing a `keydown` keyboard event,
- * followed by an asynchronous `keyup` event on a specific node.
+ * Returns a keyboard event. This event bubbles and is cancellable.
  *
- * @param {!Element} target The node to fire the event on.
- */
-export function pressSpace(target) {
-    pressAndReleaseKeyOn(target, 32);
-}
-
-/**
- * Simulates a complete key press by firing a `keydown` keyboard event, followed
- * by an asynchronous `keyup` event on a specific node.
- *
- * @param {!Element} target The node to fire the event on.
+ * @param {string} type The type of keyboard event (such as 'keyup' or
+ * 'keydown').
  * @param {number} keyCode The keyCode for the event.
  * @param {(string|Array<string>)=} modifiers The key modifiers for the event.
  *     Accepted values are shift, ctrl, alt, meta.
  * @param {string=} key The KeyboardEvent.key value for the event.
- * @return {undefined}
+ * @return {!Event}
  */
-export function pressAndReleaseKeyOn(target, keyCode, modifiers, key) {
-    keyUpOn(target, keyCode, modifiers, key);
-}
+export function keyboardEventFor(type, keyCode, modifiers, key) {
+  const event = new CustomEvent(type, {
+    detail: 0,
+    bubbles: true,
+    cancelable: true,
+    // Allow event to go outside a ShadowRoot.
+    composed: true
+  });
 
+  event.keyCode = keyCode;
+  event.code = keyCode;
 
-/**
- * Fires a 'keyup' event on a specific node. This event bubbles and is
- * cancellable.
- *
- * @param {!Element} target The node to fire the event on.
- * @param {number} keyCode The keyCode for the event.
- * @param {(string|Array<string>)=} modifiers The key modifiers for the event.
- *     Accepted values are shift, ctrl, alt, meta.
- * @param {string=} key The KeyboardEvent.key value for the event.
- * @return {undefined}
- */
-export function keyUpOn(target, keyCode, modifiers, key) {
-    keyEventOn(target, 'keyup', keyCode, modifiers, key);
+  modifiers = modifiers || [];
+  if (typeof modifiers === 'string') {
+    modifiers = [modifiers];
+  }
+  event.shiftKey = modifiers.indexOf('shift') !== -1;
+  event.altKey = modifiers.indexOf('alt') !== -1;
+  event.ctrlKey = modifiers.indexOf('ctrl') !== -1;
+  event.metaKey = modifiers.indexOf('meta') !== -1;
+
+  event.key = key;
+
+  return event;
 }
 
 /**
@@ -53,43 +50,45 @@ export function keyUpOn(target, keyCode, modifiers, key) {
  * @return {undefined}
  */
 export function keyEventOn(target, type, keyCode, modifiers, key) {
-    target.dispatchEvent(keyboardEventFor(type, keyCode, modifiers, key));
+  target.dispatchEvent(keyboardEventFor(type, keyCode, modifiers, key));
 }
 
-
 /**
- * Returns a keyboard event. This event bubbles and is cancellable.
+ * Fires a 'keyup' event on a specific node. This event bubbles and is
+ * cancellable.
  *
- * @param {string} type The type of keyboard event (such as 'keyup' or
- * 'keydown').
+ * @param {!Element} target The node to fire the event on.
  * @param {number} keyCode The keyCode for the event.
  * @param {(string|Array<string>)=} modifiers The key modifiers for the event.
  *     Accepted values are shift, ctrl, alt, meta.
  * @param {string=} key The KeyboardEvent.key value for the event.
- * @return {!Event}
+ * @return {undefined}
  */
-export function keyboardEventFor(type, keyCode, modifiers, key) {
-    const event = new CustomEvent(type, {
-        detail: 0,
-        bubbles: true,
-        cancelable: true,
-        // Allow event to go outside a ShadowRoot.
-        composed: true
-    });
+export function keyUpOn(target, keyCode, modifiers, key) {
+  keyEventOn(target, 'keyup', keyCode, modifiers, key);
+}
 
-    event.keyCode = keyCode;
-    event.code = keyCode;
+/**
+ * Simulates a complete key press by firing a `keydown` keyboard event, followed
+ * by an asynchronous `keyup` event on a specific node.
+ *
+ * @param {!Element} target The node to fire the event on.
+ * @param {number} keyCode The keyCode for the event.
+ * @param {(string|Array<string>)=} modifiers The key modifiers for the event.
+ *     Accepted values are shift, ctrl, alt, meta.
+ * @param {string=} key The KeyboardEvent.key value for the event.
+ * @return {undefined}
+ */
+export function pressAndReleaseKeyOn(target, keyCode, modifiers, key) {
+  keyUpOn(target, keyCode, modifiers, key);
+}
 
-    modifiers = modifiers || [];
-    if (typeof modifiers === 'string') {
-        modifiers = [modifiers];
-    }
-    event.shiftKey = modifiers.indexOf('shift') !== -1;
-    event.altKey = modifiers.indexOf('alt') !== -1;
-    event.ctrlKey = modifiers.indexOf('ctrl') !== -1;
-    event.metaKey = modifiers.indexOf('meta') !== -1;
-
-    event.key = key;
-
-    return event;
+/**
+ * Simulates a complete 'space' key press by firing a `keydown` keyboard event,
+ * followed by an asynchronous `keyup` event on a specific node.
+ *
+ * @param {!Element} target The node to fire the event on.
+ */
+export function pressSpace(target) {
+  pressAndReleaseKeyOn(target, 32);
 }
