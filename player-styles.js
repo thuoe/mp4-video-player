f686b1a7fc36a844cc2953593040614c673e7cfd
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2020-05-29 23:18:17 +0100

    position large play button

--- player-styles.js
+++ player-styles.js
@@ -55,6 +55,23 @@ const playerStyles = html`
       opacity: 1;
     }
 
+    .large-btn {
+      position: absolute;
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+      background: var(--video-large-play-button-color, #d32f2f);
+      cursor: pointer;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      transition: background .2s ease-in-out;
+    }
+
+    .large-btn:hover {
+      background: var(--video-large-play-button--hover-color, #9a0007);
+    }
+
     .container {
       position: relative;
       display: flex;
