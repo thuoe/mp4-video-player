0e7206448ef30ea7e1a6ecfdaba0ec90d7b51683
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2020-05-31 21:27:36 +0100

    add dynamic click function for testing

--- test/Interactions.js
+++ test/Interactions.js
@@ -1,4 +1,15 @@
 /* eslint-disable no-param-reassign */
+
+const HAS_NEW_MOUSE = (() => {
+  let has = false;
+  try {
+    has = Boolean(new MouseEvent('x'));
+  } catch (_) {
+    // do nothing
+  }
+  return has;
+})();
+
 /**
  * Returns a keyboard event. This event bubbles and is cancellable.
  *
@@ -91,4 +102,75 @@ export function pressAndReleaseKeyOn(target, keyCode, modifiers, key) {
  */
 export function pressSpace(target) {
   pressAndReleaseKeyOn(target, 32);
+}
+
+/**
+ * Fires a mouse event on a specific node, at a given set of coordinates.
+ * This event bubbles and is cancellable.
+ *
+ * @param {string} type The type of mouse event (such as 'tap' or 'down').
+ * @param {{ x: number, y: number }} xy The (x,y) coordinates the mouse event
+ * should be fired from.
+ * @param {!Element} node The node to fire the event on.
+ * @return {undefined}
+ */
+export function makeMouseEvent(type, xy, node) {
+  const props = {
+    bubbles: true,
+    cancelable: true,
+    clientX: xy.x,
+    clientY: xy.y,
+    // Allow event to go outside a ShadowRoot.
+    composed: true,
+    // Make this a primary input.
+    buttons:
+        1 // http://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/buttons
+  };
+  let e;
+  if (HAS_NEW_MOUSE) {
+    e = new MouseEvent(type, props);
+  } else {
+    e = document.createEvent('MouseEvent');
+    e.initMouseEvent(
+      type,
+      props.bubbles,
+      props.cancelable,
+      null, /* view */
+      null, /* detail */
+      0, /* screenX */
+      0, /* screenY */
+      props.clientX,
+      props.clientY,
+      false, /* ctrlKey */
+      false, /* altKey */
+      false, /* shiftKey */
+      false, /* metaKey */
+      0, /* button */
+      null /* relatedTarget */
+    );
+  }
+  node.dispatchEvent(e);
+}
+
+/**
+ * Returns the (x,y) coordinates representing the middle of a node.
+ *
+ * @param {!Element} node An element.
+ * @return {{x: number, y:number}}
+ */
+export function middleOfNode(node) {
+  const bcr = node.getBoundingClientRect();
+  return { y: bcr.top + (bcr.height / 2), x: bcr.left + (bcr.width / 2) };
+}
+
+/**
+ * Generate a click event on a given node, optionally at a given coordinate.
+ * @param {!Element} node The node to fire the click event on.
+ * @param {{ x: number, y: number }=} xy Optional. The (x,y) coordinates the
+ * mouse event should be fired from.
+ * @return {undefined}
+ */
+export function click(node, xy) {
+  xy = xy || middleOfNode(node);
+  makeMouseEvent('click', xy, node);
 }
